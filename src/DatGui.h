8f925fbcd7ba468284a5f94aa424bc28be9269c3
Nicolás Venturo <nicolas.venturo@gmail.com>
2016-07-10 16:40:26 -0300

    Added special header to prevent ofxDatGui from triggering warnings

---
+++ src/DatGui.h
@@ -0,0 +1,16 @@
+#ifndef _DAT_GUI_H
+#define _DAT_GUI_H
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-value"
+#endif
+
+#include "ofxDatGui.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+#endif // _DAT_GUI_H
